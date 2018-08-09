0d9963b60ea87bad27ae5426b130f802b982ba13
zude <zude@seeapp.com>
2018-08-09 15:20:07 +0800

    Initial commit

---
+++ src/main/java/com/xufx/Interfaces/Command.java
@@ -0,0 +1,11 @@
+package com.xufx.Interfaces;
+
+/**
+ * 命令接口，声明执行的操作
+ */
+public interface Command {
+    /**
+     * 执行命令对应的操作
+     */
+    public void execute();
+}
---
+++ src/main/java/com/xufx/Interfaces/MainBoardApi.java
@@ -0,0 +1,11 @@
+package com.xufx.Interfaces;
+
+/**
+ * 主板的接口
+ */
+public interface MainBoardApi {
+    /**
+     * 主板具有能开机的功能
+     */
+    public void open();
+}
